0af29146ebb5755e05e3ba35f8a165e088956e14
guiguegon <guiguegon@gmail.com>
2015-06-30 19:25:57 +0200

    put and delete methods

--- routes/planetas.js
+++ routes/planetas.js
@@ -49,6 +49,52 @@ router.route('/')
   			res.status(400);
   			res.end('Bad request');
   		}
+  })
+  .put(function(req, res) {
+    if(req.body.planeta !== undefined) {
+      var nuevoPlaneta = req.body.planeta;
+      var id = req.params.id;
+      nuevoPlaneta.id = parseInt(id, 10);
+
+      if(planetas[id] !== undefined) {
+          planetas[id] = nuevoPlaneta;
+          res.set('Content-Type', 'application/json');
+          res.status(200);
+          res.json({
+            planeta: nuevoPlaneta
+          });
+      } else {
+        console.log('planeta with id ' + id + ' not found');
+        res.status(404);
+        res.end('not found');
+      }
+
+    } else {
+      console.log('body empty');
+      res.status(400);
+      res.end('bad request');
+    }
+
+  })
+  .delete(function(req, res) {
+    var id = req.params.id;
+    if(id !== undefined) {
+      if(planetas[id] !== undefined) {
+          delete planetas[id];
+          res.json({
+        		'planetas':planetas
+        	});
+      } else {
+        console.log('planeta with id ' + id + ' not found');
+        res.status(404);
+        res.end('not found');
+      }
+
+    } else {
+      console.log('id empty');
+      res.status(400);
+      res.end('bad request');
+    }
   });
 
 
--- test/planetas.spec.js
+++ test/planetas.spec.js
@@ -1,10 +1,10 @@
-var request = require('supertest');
+var request = require('supertest-as-promised');
 var api = require('../server.js');
 var expect = require('chai').expect;
 
 request = request(api);
 
-describe('TEST de Planetas', function() {
+describe('Test de Planetas', function() {
 
   describe('POST /planetas', function() {
 
@@ -62,33 +62,118 @@ describe('TEST de Planetas', function() {
       .post('/planetas')
       .set('Accept', 'application/json')
       .send(data)
-      .end(function(err, res) {
-        if(err) {
-          done(err);
-        }
+      .then(function getPlanetas(res) {
 
-        request
-        .get('/planetas')
-        .set('Accept', 'application/json')
-        .send()
-        .expect(200)
-        .expect('Content-Type', /application\/json/)
-        .end(function(err, res) {
-          var planeta;
+        return request.get('/planetas')
+          .set('Accept', 'application/json')
+          .send()
+          .expect(200)
+          .expect('Content-Type', /application\/json/)
+        }, done)
+      .then(function assertion(res) {
+        var planeta
+        var body = res.body;
+        console.log('body', body);
 
-          var body = res.body;
-          console.log('body', body);
+        //Planeta existe
+        expect(body).to.have.property('planetas');
+        planetas = body.planetas;
 
-          //Planeta existe
-          expect(body).to.have.property('planetas');
-          planetas = body.planetas;
-
-          //Propiedades
-          expect(planetas).not.to.be.empty;
-          done(err);
-      });
-    });
+        //Propiedades
+        expect(planetas).not.to.be.empty;
+        done();
+      }, done);
 
     });
   });
+
+  // TDD PUT
+  describe('PUT /planetas/:id', function() {
+      it('deberia actualizar un planeta existente', function (done) {
+        var data = {
+          "planeta": {
+            "nombre": "Mercurio",
+          }
+        };
+
+        request
+        .post('/planetas')
+        .set('Accept', 'application/json')
+        .send(data)
+        .expect(201)
+        .expect('Content-Type', /application\/json/)
+        .then(function putPlaneta(res) {
+
+          var update = {
+          "planeta": {
+            "nombre": "Venus",
+          }
+        };
+
+        id = res.body.planeta.id;
+
+        return request.put('/planetas/' + id)
+                 .set('Accept', 'application/json')
+                 .send(update)
+                 .expect(200)
+                 .expect('Content-Type', /application\/json/);
+        }, done)
+        .then(function assertions(res) {
+          var planeta;
+          var body = res.body;
+          console.log('GET body', body);
+          // Planeta existe
+          expect(body).to.have.property('planeta');
+          planeta = body.planeta;
+
+          // Propiedades
+          expect(planeta).to.have.property('id', id);
+          expect(planeta).to.have.property('nombre', 'Venus');
+
+          done();
+        }, done);
+
+      });
+    });
+
+    // TDD DELETE
+    describe('DELETE /planetas/:id', function() {
+        it('deberia eliminar un planeta existente', function (done) {
+          var id;
+          var data = {
+            "planeta": {
+              "nombre": "Mercurio",
+            }
+          };
+
+          request
+          .post('/planetas')
+          .set('Accept', 'application/json')
+          .send(data)
+          .expect(201)
+          .expect('Content-Type', /application\/json/)
+          .then(function deletePlaneta(res) {
+
+          id = res.body.planeta.id;
+
+          return request.delete('/planetas/' + id)
+                   .set('Accept', 'application/json')
+                   .send()
+                   .expect(200)
+                   .expect('Content-Type', /application\/json/);
+          }, done)
+          .then(function assertions(res) {
+            var body = res.body;
+            console.log('GET body', body);
+            // Planeta no existe
+            expect(body).to.have.property('planetas');
+            planetas = body.planetas;
+
+            expect(planetas).to.not.have.property(id);
+
+            done();
+          }, done);
+
+        });
+      });
 });
